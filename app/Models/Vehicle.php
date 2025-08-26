b061c4b4475d4b4b15d8eafc080531263e1745f7
Kaua Rodrigues <kauarodrigues.fn@gmail.com>
2025-08-26 09:34:38 -0300

    feat: adiciona atributos fillable e casts no model Vehicle

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    //
+    protected $fillable = [
+        'external_id', 'type', 'brand', 'model', 'version',
+        'year_model', 'year_build', 'optionals', 'doors',
+        'board', 'chassi', 'transmission', 'km', 'description',
+        'sold', 'category', 'url_car', 'old_price', 'price',
+        'color', 'fuel', 'photos',
+    ];
+
+    protected $casts = [
+        'optionals' => 'array',
+        'photos' => 'array',
+        'sold' => 'boolean',
+        'price' => 'decimal:2',
+        'old_price' => 'decimal:2',
+        'doors' => 'integer',
+        'km' => 'integer',          
+    ];
 }
